52ea53e891e71a517d5e663a645cb0c88e4309d0
MohammedEshan <105197792+MohammedEshan@users.noreply.github.com>
2025-05-18 16:05:32 +0530

    Create ddl_bronze.sql

---
+++ scripts/Bronze/ddl_bronze.sql
@@ -0,0 +1,9 @@
+/*
+===========================================================================================================
+DDL Script : Create Bronze Tables
+===========================================================================================================
+Script Purpose :
+          This Script Creates Tables in 'Bronze Schema' dropping existing tables 
+          if they already exists 
+
+*/
